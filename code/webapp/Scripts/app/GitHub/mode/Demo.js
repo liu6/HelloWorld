88bec3c8d7adfdf7d97b88b6af083d42fbee2ec7
六六 <liu6e@qq.com>
2013-12-03 21:38:45 +0800

    Create Demo.js

---
+++ code/webapp/Scripts/app/GitHub/mode/Demo.js
@@ -0,0 +1,10 @@
+Ext.define('app.mode.Demo', {
+    extend: 'Ext.data.Model',
+    fields: [{
+        name: 'title',
+        type: 'string'
+    }, {
+        name: 'year',
+        type: 'int'
+    }]
+});
